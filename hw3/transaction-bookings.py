fd9fb678430666fd79d289bed8a8cd063c048c77
Dion <53874292+dion-shushi@users.noreply.github.com>
2020-11-01 12:53:36 -0600

    check comment at top

--- hw3/transaction-bookings.py
+++ hw3/transaction-bookings.py
@@ -2,6 +2,35 @@ import psycopg2, sys
 import logging
 import threading
 import datetime
+import time
+
+"""
+    THINGS TO KEEP IN MIND
+    1. book_date column in table "bookings" should be on or before the sheduled_departure date in the "flights" table
+    2. count the number of succesful and unsuccessful transactions (confused)
+        =>this should be simple for nonTrans()
+            ->can run "select seats_available from flights where flight_id = same" => fetch => see if value is 0 or not [0=fail, >0=success]
+        =>don't understand for yesTrans() since all the sql has to run once (????) 
+            -> thought 
+                --> before anything 
+                --> run a sql to check if seats_available is 0 
+                --> if it is then terminate the sql query
+                --> does terminating return anything to python? psycopg2?
+                --> links:
+                    -- https://stackoverflow.com/questions/10377781/return-boolean-value-on-sql-select-statement 
+                    -- https://stackoverflow.com/questions/2862080/sql-return-true-if-list-of-records-exists/2862117
+    3. TRANSACTIONS SHOULD BE SPREAD BETWEEN THREADS AS EVENLY AS POSSIBLE (i think? whats are we outputting?)
+        => think output is:
+            Successful transaction: #
+            Unsuccessful transaction: #
+    4. no need to measure the time completion for the program
+
+    TEST CASES
+    1. normal cases 10-20 threads
+    2. hard cases >= 100 threads and >= 10000 input lines   
+
+
+"""
 
 def connectWithDB():
     with open('password.txt') as f:
@@ -16,10 +45,6 @@ def connectWithDB():
     server_var = [cursor, conn]
 
     return server_var
-
-def thread_function(name):
-    logging.info("Thread %s: starting", name)
-    logging.info("Thread %s: finishing", name)
 
 def callThread():
     format = "%(asctime)s: %(message)s"
@@ -44,11 +69,12 @@ def addZero(singleDigit):
     return toReturn
 
 def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
+    print(lines[0] + " " + lines[1] + " executed")
     """
         y_m_d is a array with [year, month, day]
         m_d_str is a array with [month, day] both data is in the data type string
         
-        if month and day is a single digit => then add a '0' in from of it
+        if month and day is a single digit => then add a '0' in front of it
     """
     if y_m_d[0] > 2020:
                 y_m_d[0]= 2000
@@ -61,12 +87,10 @@ def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
         m_d_str[0] = addZero(y_m_d[1])
     if y_m_d[2] < 10:
         m_d_str[1] = addZero(y_m_d[2])
-    
-    #split the line with ','
 
-    #if the string before the first ',' is a digit
-    
-    sql = "start transaction;"
+    sql = "declare"
+    sql += " temp int;"
+    sql += " begin"
     sql += " update flights"
     sql += " set seats_available = case"
     sql += " when seats_available > 0 and flight_id = " + lines[1]
@@ -77,12 +101,20 @@ def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
     sql += " end;"
     sql += " insert into bookings"
     sql += " values(" + str(book_ref) + ", TIMESTAMP '" + str(y_m_d[0]) + "-" + m_d_str[0] + "-" + m_d_str[1] + " 00:00:00-05'" + ", 127000);"  
-    sql += " insert into ticket"
-    sql += " values(" + str(ticket_no) + ", " + str(book_ref) + ", " + lines[1] + ", null, null, null);"
-    sql += " commit;"
+    sql += " insert into ticket(ticket_no, book_ref, passenger_id)"
+    sql += " values(" + str(ticket_no) + ", " + str(book_ref) + ", " + lines[1] + ");"
+    sql += " exception"
+    sql += " when seats_available = 0 then rollback, select case when exists(select * from flights where seats_available = 0) then cast (1 as bit)"
+    sql += " when seats_available > 0 then commit, select case when exists(select * from flights where seats_available > 0) then cast (0 as bit);"
+    sql += " end;"
     server_var[0].execute(sql)
-
-        
+    fetch = server_var[0].fetchall()
+    print(fetch)
+    if fetch[0] == 1:
+        print("success")
+    elif fetch[0] == 0:
+        print("fail")
+    # time.sleep(1)
 
 def yesTrans(server_var, fileName, threadNum):
     file = open(fileName, 'r')
@@ -98,8 +130,10 @@ def yesTrans(server_var, fileName, threadNum):
         if line[0].isdigit(): 
             lines.append(string) 
 
-    threads = []
-    for line in range(len(lines)):
+    line = 0
+    while line < len(lines):
+        print(line)
+        threads = []
         for i in range(int(threadNum)):
             y_m_d[0] = y_m_d[0]+1
             y_m_d[1] = y_m_d[1]+1
@@ -110,14 +144,16 @@ def yesTrans(server_var, fileName, threadNum):
             x = threading.Thread(target=yesTransThread, args=(server_var, book_ref, ticket_number, y_m_d, m_d_str, lines[line]))
             if line < len(lines)-1:
                 print(str(line) + " updated to " + str(line+1))
-                line = line+1
+                line+=1
                 print("line:", line, ", range:", len(lines))
             x.start()
             threads.append(x)
+            print("x got appended")
         
         for thread in range(len(threads)):
             print("thread #", thread, "joined")
             threads[thread].join()
+        line+=1
 
 def noTrans(server_var, fileName, threadNum):
     file = open(fileName, 'r')
@@ -149,7 +185,13 @@ def updateDB(server_var, inputList):
     sql += "delete from bookings;"
     sql += " update flights "
     sql += " set seats_available = 50, seats_booked = 0; commit;"
+    # sql = "select case when exists ("
+    # sql += " select * from flights where seats_available = 0 and flight_id = 1001)"
+    # sql += " then cast(1 as bit)"
+    # sql += " else cast(0 as bit) end;"
     server_var[0].execute(sql)
+    # fetch = server_var[0].fetchall()
+    # print(fetch)
 
     if inputList[1] == 'n':
         noTrans(server_var, inputList[0], inputList[2]) #(fileName, # of threads)
