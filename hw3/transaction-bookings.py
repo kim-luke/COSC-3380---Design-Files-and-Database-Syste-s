f3e40b556df2bc7427b523835a8cd125718d39b2
kim-luke <48031366+kim-luke@users.noreply.github.com>
2020-11-01 22:37:14 -0600

    Update transaction-bookings.py

--- hw3/transaction-bookings.py
+++ hw3/transaction-bookings.py
@@ -29,6 +29,8 @@ import time
     2. hard cases >= 100 threads and >= 10000 input lines   
 """
 
+queryList = ""
+
 def connectWithDB():
     with open('password.txt') as f:
         lines = [line.rstrip() for line in f]
@@ -65,7 +67,7 @@ def addZero(singleDigit):
     toReturn = '0' + str(singleDigit)
     return toReturn
 
-def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
+def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines, queryList, outputFile):
     print(lines[0] + " " + lines[1] + " executed")
     """
         y_m_d is a array with [year, month, day]
@@ -86,24 +88,25 @@ def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
         m_d_str[1] = addZero(y_m_d[2])
 
     sql = "declare"
-    sql += " temp int;"
-    sql += " begin"
-    sql += " update flights"
-    sql += " set seats_available = case"
-    sql += " when seats_available > 0 and flight_id = " + lines[1]
-    sql += " then seats_available-1 else seats_available"
-    sql += " end,"
-    sql += " seats_booked = case "
-    sql += " when flight_id = " + lines[1] +  " then seats_booked+1 else seats_booked"
-    sql += " end;"
-    sql += " insert into bookings"
-    sql += " values(" + str(book_ref) + ", TIMESTAMP '" + str(y_m_d[0]) + "-" + m_d_str[0] + "-" + m_d_str[1] + " 00:00:00-05'" + ", 12700);"  
-    sql += " insert into ticket(ticket_no, book_ref, passenger_id)"
-    sql += " values(" + str(ticket_no) + ", " + str(book_ref) + ", " + lines[1] + ");"
-    sql += " exception"
-    sql += " when seats_available = 0 then rollback, select case when exists(select * from flights where seats_available = 0) then cast (1 as bit)"
-    sql += " when seats_available > 0 then commit, select case when exists(select * from flights where seats_available > 0) then cast (0 as bit);"
-    sql += " end;"
+    sql += " \ntemp int;"
+    sql += " \nbegin"
+    sql += " \nupdate flights"
+    sql += " \nset seats_available = case"
+    sql += " \nwhen seats_available > 0 and flight_id = " + lines[1]
+    sql += " \nthen seats_available-1 else seats_available"
+    sql += " \nend,"
+    sql += " \nseats_booked = case "
+    sql += " \nwhen flight_id = " + lines[1] +  " then seats_booked+1 else seats_booked"
+    sql += " \nend;"
+    sql += " \ninsert into bookings"
+    sql += " \nvalues(" + str(book_ref) + ", TIMESTAMP '" + str(y_m_d[0]) + "-" + m_d_str[0] + "-" + m_d_str[1] + " 00:00:00-05'" + ", 12700);"  
+    sql += " \ninsert into ticket(ticket_no, book_ref, passenger_id)"
+    sql += " \nvalues(" + str(ticket_no) + ", " + str(book_ref) + ", " + lines[1] + ");"
+    sql += " \nexception"
+    sql += " \nwhen seats_available = 0 then rollback, select case when exists(select * from flights where seats_available = 0) then cast (1 as bit)"
+    sql += " \nwhen seats_available > 0 then commit, select case when exists(select * from flights where seats_available > 0) then cast (0 as bit);"
+    sql += " \nend;\n"
+    queryList += sql
     server_var[0].execute(sql)
     fetch = server_var[0].fetchall()
     print(fetch)
@@ -112,8 +115,9 @@ def yesTransThread(server_var, book_ref, ticket_no, y_m_d, m_d_str, lines):
     elif fetch[0] == 0:
         print("fail")
     # time.sleep(1)
+    outputFile.write(queryList)
 
-def yesTrans(server_var, fileName, threadNum):
+def yesTrans(server_var, fileName, threadNum, queryList, outputFile):
     file = open(fileName, 'r')
     book_ref = 0
     ticket_number = 6000000000
@@ -138,7 +142,7 @@ def yesTrans(server_var, fileName, threadNum):
             book_ref = book_ref+1
             ticket_number = ticket_number+1
             print("creating thread #", i)
-            x = threading.Thread(target=yesTransThread, args=(server_var, book_ref, ticket_number, y_m_d, m_d_str, lines[line]))
+            x = threading.Thread(target=yesTransThread, args=(server_var, book_ref, ticket_number, y_m_d, m_d_str, lines[line], queryList, outputFile))
             if line < len(lines)-1:
                 print(str(line) + " updated to " + str(line+1))
                 line+=1
@@ -152,23 +156,18 @@ def yesTrans(server_var, fileName, threadNum):
             threads[thread].join()
         line+=1
 
-def noTrans(server_var, fileName, threadNum):
+def noTrans(server_var, fileName, threadNum, queryList, outputFile):
     file = open(fileName, 'r')
     book_ref = 60000
     ticket_no = 6000000000
     y_m_d = [2003, 1, 2]
     m_d_str = ["", ""]
+    flight_id = ""
     for i in file:
         string = i.split(',')
-        if string[0].isdigit():
+        if string[0].isdigit():   
 
-            """
-            sql = "SELECT flights.scheduled_departure FROM flights WHERE seats_available > 0 AND flight_id = " + string[1] + ";"
-            server_var[0].execute(sql)
-            fetch = server_var[0].fetchall()
-            print(fetch)
-            """
-            
+            flight_id = string[1].rstrip()
             
             # sql = "SELECT * FROM flights WHERE seats_available = 50 AND flight_id = " + string[1] + ";"
             # sql = "SELECT flights.scheduled_departure FROM flights WHERE seats_available > 0 AND flight_id = " + string[1] + ";"
@@ -187,77 +186,102 @@ def noTrans(server_var, fileName, threadNum):
             if y_m_d[2] < 10:
                 m_d_str[1] = addZero(y_m_d[2])
 
-            
-            sql = " INSERT INTO bookings"
-            sql += " VALUES(" + str(book_ref) + ", TIMESTAMP '" + str(y_m_d[0]) + "-" + m_d_str[0] + "-" + m_d_str[1] + " 00:00:00-05'" + ", 12700);" 
-            server_var[0].execute(sql)
-
-            sql = " SELECT * FROM bookings"
-            server_var[0].execute(sql)
-            #fetch = server_var[0].fetchall()
-            #print(fetch)         
-            
-            sql = " INSERT INTO ticket(ticket_no, book_ref, passenger_id, passenger_name)"
-            sql += " VALUES(" + str(ticket_no) + ", " + str(book_ref) + ", " + string[0] + ", " + "'LUKE'" + ");"
-            server_var[0].execute(sql)
-
-            sql = " SELECT * FROM ticket"
+            sql = "\nSELECT seats_available FROM flights WHERE flight_id = " + flight_id + ";\n"
+            queryList += sql
             server_var[0].execute(sql)
             fetch = server_var[0].fetchall()
-            print(fetch)
+            temp = fetch
+            store = temp[0][0]  
             
-            sql = " INSERT INTO ticket_flights(ticket_no, flight_id, fare_conditions, amount)"
-            sql += " VALUES(" + str(ticket_no) + ", " + string[1] + ", " + "'Economy', " + "'12700');"
+            sql = " \nINSERT INTO bookings"
+            sql += " \nVALUES(" + str(book_ref) + ", TIMESTAMP '" + str(y_m_d[0]) + "-" + m_d_str[0] + "-" + m_d_str[1] + " 00:00:00-05'" + ", 12700);\n" 
             server_var[0].execute(sql)
+            queryList += sql
 
-            sql = " SELECT * FROM ticket_flights"
-            server_var[0].execute(sql)
-            fetch = server_var[0].fetchall()
-            ##print(fetch)
+            if (store > 0):
+
+                sql = " \nINSERT INTO ticket(ticket_no, book_ref, passenger_id, passenger_name)"
+                sql += " \nVALUES(" + str(ticket_no) + ", " + str(book_ref) + ", " + string[0] + ", " + "' '" + ");\n"
+                server_var[0].execute(sql)
+                queryList += sql
+
+                """
+                sql = " SELECT * FROM ticket"
+                server_var[0].execute(sql)
+                fetch = server_var[0].fetchall()
+                print(fetch)
+                """
+            
+                sql = " \nINSERT INTO ticket_flights(ticket_no, flight_id, fare_conditions, amount)"
+                sql += " \nVALUES(" + str(ticket_no) + ", " + flight_id + ", " + "'Economy', " + "'12700');\n"
+                server_var[0].execute(sql)
+                queryList += sql
+
+                sql = " \nUPDATE flights"
+                sql += " \nSET seats_available = seats_available - 1, seats_booked = seats_booked + 1 WHERE flight_id = " + flight_id + ";\n"
+                server_var[0].execute(sql)
+                queryList += sql
+
+                sql = " \nSELECT seats_available FROM flights WHERE flight_id = " + flight_id + ";\n"
+                server_var[0].execute(sql)
+                fetch = server_var[0].fetchall()
+                temp = fetch
+                store = temp[0][0]    
+                queryList += sql
+
+                # print(string[1] + " " + str(store)) 
+                # print(book_ref)
+                #print(flight_id)
+
+            else:
+                print("no seats available!")  
+                # print(book_ref)
                 
-            #print(string[0])
-            #print(string[1])
-
-            print(book_ref)
         book_ref = book_ref + 1
         y_m_d[0] = y_m_d[0] + 1
         y_m_d[1] = y_m_d[1] + 1
         y_m_d[2] = y_m_d[2] + 1
-        ticket_no = ticket_no + 1
-            
+        ticket_no = ticket_no + 1   
 
+    outputFile.write(queryList)
+
+    #print(store)
 
         # 1. generate book_ref number, update bookings
         # 2. generate ticket_num, insert new record in ticket and ticket_flights IF available seats in specified flight AND date
         # 3. update seats_booked and seats_available in flights (simple)
         # 4. IF no available seats, ONLY book_ref generated and bookings updated
  
-def updateDB(server_var, inputList):
+def updateDB(server_var, inputList, queryList, outputFile):
 
     #delete all the rows from all the tables
-    sql = "truncate ticket CASCADE; "
-    sql += "delete from ticket_flights;"
-    sql += "delete from bookings;"
-    sql += " update flights "
-    sql += " set seats_available = 50, seats_booked = 0; commit;"
+    sql = "TRUNCATE ticket CASCADE; "
+    sql += "\ndelete from ticket_flights;"
+    sql += "\ndelete from bookings;"
+    sql += " \nupdate flights "
+    sql += " \nset seats_available = 50, seats_booked = 0; \ncommit;\n"
     # sql = "select case when exists ("
     # sql += " select * from flights where seats_available = 0 and flight_id = 1001)"
     # sql += " then cast(1 as bit)"
     # sql += " else cast(0 as bit) end;"
     server_var[0].execute(sql)
+    queryList += sql
     # fetch = server_var[0].fetchall()
     # print(fetch)
 
     if inputList[1] == 'n':
-        noTrans(server_var, inputList[0], inputList[2]) #(fileName, # of threads)
+        noTrans(server_var, inputList[0], inputList[2],  queryList, outputFile) #(fileName, # of threads)
     if inputList[1] == 'y':
-        yesTrans(server_var, inputList[0], inputList[2]) #(fileName, # of threads)
+        yesTrans(server_var, inputList[0], inputList[2], queryList, outputFile) #(fileName, # of threads)
+
+    outputFile.write(queryList)
 
 def main(argv):
     inputs = []
+    outputFile = open("checkdb.sql", "w")
 
     indexOfEq = argv.find('=')
-    indexOfSemiFirst = argv.find(';')
+    indexOfSemiFirst = argv.find(';') 
     
     #file name
     inputFileName = argv[indexOfEq+1:indexOfSemiFirst]
@@ -275,7 +299,8 @@ def main(argv):
     inputs.append(inputThreadNum)
 
     returned_server_var = connectWithDB()
-    updateDB(returned_server_var, inputs)
+    updateDB(returned_server_var, inputs, queryList, outputFile)
+    outputFile.close()
     
 
 if __name__ == "__main__":
